2a3cde4a0a29a1bf573e59a573c7f4bbe388a1c5
thelastenvoy <satvikbatra@gmail.com>
2023-10-11 15:21:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 const fs = require('fs');
-const csv = require('csv-parser');
+const csvParser = require('csv-parser');
 
-const csvFilePath = process.argv[2];
-const columnName = process.argv[3];
+const args = process.argv.slice(2);
+const filePath = args[0]; // CSV file path
+const columnName = args[1]; // Column name to sum
+
+if (!filePath || !columnName) {
+  console.error('Usage: node sum_csv_column.js <path_to_csv_file> <column_name>');
+  process.exit(1);
+}
 
 let sum = 0;
 
-fs.createReadStream(csvFilePath)
-  .pipe(csv())
-  .on('data', (data) => {
-    // TODO: Add code to sum values in the specified column
+fs.createReadStream(filePath)
+  .pipe(csvParser())
+  .on('data', (row) => {
+    const value = parseFloat(row[columnName]);
+    if (!isNaN(value)) {
+      sum += value;
+    }
   })
   .on('end', () => {
+    if (sum == 0) {
+      console.log('Invalid column name');
+      return;
+    }
     console.log(`The sum of ${columnName} is: ${sum}`);
-});
+  })
+  .on('error', (error) => {
+    console.error('Error reading the CSV file:', error);
+  });
