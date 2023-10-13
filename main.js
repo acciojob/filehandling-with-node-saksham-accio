1e1bbb4704cd0685786d18ffd2c81d15fa689ccc
thelastenvoy <satvikbatra@gmail.com>
2023-10-13 15:12:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 const fs = require('fs');
 const csvParser = require('csv-parser');
 
-const args = process.argv.slice(2);
-const filePath = args[0]; // CSV file path
-const columnName = args[1]; // Column name to sum
+function sumCSVColumn(filePath, columnName, callback) {
+  if (!filePath || !columnName) {
+    callback('Usage: node main.js <path_to_csv_file> <column_name>', null);
+    return;
+  }
 
-if (!filePath || !columnName) {
-  console.error('Usage: node sum_csv_column.js <path_to_csv_file> <column_name>');
-  process.exit(1);
+  let sum = 0;
+    
+  let stream = fs.createReadStream(filePath);
+
+  stream.on('error', function(err) {
+      callback(`Error reading the CSV file: ${err}`, null);
+      return; // Ensure that the rest of the code doesn't execute
+  });
+  
+  stream.pipe(csvParser())
+    .on('data', (row) => {
+        const value = parseFloat(row[columnName]);
+        if (!isNaN(value)) {
+          sum += value;
+        }
+    })
+    .on('end', () => {
+        if (sum == 0) {
+            callback('Invalid column name', null);
+            return;
+          }
+          callback(null, `The sum of ${columnName} is: ${sum}`);
+    });
+
 }
 
-let sum = 0;
-
-fs.createReadStream(filePath)
-  .pipe(csvParser())
-  .on('data', (row) => {
-    const value = parseFloat(row[columnName]);
-    if (!isNaN(value)) {
-      sum += value;
-    }
-  })
-  .on('end', () => {
-    if (sum == 0) {
-      console.log('Invalid column name');
-      return;
-    }
-    console.log(`The sum of ${columnName} is: ${sum}`);
-  })
-  .on('error', (error) => {
-    console.error('Error reading the CSV file:', error);
-  });
+module.exports = sumCSVColumn;
